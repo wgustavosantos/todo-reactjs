d9b959a49916e04f05a06da63dccdea6b3dbc8a5
Wenderson Gustavo <guto15santos@gmail.com>
2023-08-10 14:02:25 -0300

    refatorando codigo

--- src/componentes/Todo/Todo.jsx
+++ src/componentes/Todo/Todo.jsx
@@ -1,13 +1,22 @@
 import './Todo.css'
+import iconCheckSvg from '../../assets/img/icon-check.svg';
+
+
 
 const Todo = ({ todo, removeTodo, completeTodo }) => {
 
-    const backgroundCompleted = 'url("/src/assets/img/icon-check.svg"), linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))';
+
+    const backgroundCompleted = `url(${iconCheckSvg}), linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))`;
+
+    function completeTodoFunc (id){
+        completeTodo(id);
+        
+    }
 
     return (
         <div className='todo'>
 
-            <button style={{backgroundImage: todo.isCompleted ? backgroundCompleted : " "}} onClick={() => completeTodo(todo.id)} className='complete'></button>
+            <button style={{backgroundImage: todo.isCompleted ? backgroundCompleted : ''}} onClick={() => completeTodoFunc(todo.id)} className='complete'></button>
 
             <p
             style={

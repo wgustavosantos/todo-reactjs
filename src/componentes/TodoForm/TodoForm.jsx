fd3d7a5ffbcad0dd1b24a11b74c6bfdb03b4ee80
Wenderson Gustavo <guto15santos@gmail.com>
2023-08-12 16:13:59 -0300

    Salvando todo no localstorage

--- src/componentes/TodoForm/TodoForm.jsx
+++ src/componentes/TodoForm/TodoForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './TodoForm.css'
 import NewTodo from '../NewTodo/NewTodo';
@@ -9,6 +9,7 @@ import { SortableItem } from '../SortableItems/SortableItems';
 import {
     DndContext,
     closestCenter
+
 } from '@dnd-kit/core'
 
 import {
@@ -19,39 +20,44 @@ import {
 
 const TodoForm = () => {
 
-    const [todos, setTodos] = useState([
-        {
-            id: uuidv4(),
-            text: 'Estudar Dor Pélvica Crônica',
-            isCompleted: false
-        },
-        {
-            id: uuidv4(),
-            text: 'Revisar Incontinência Urinária e Disfunções Pélvicas',
-            isCompleted: false
-        },
-        {
-            id: uuidv4(),
-            text: 'assistir 2 aulas do curso Gestão da Dor no Câncer Ginecológico',
-            isCompleted: false
-        },
-        {
-            id: uuidv4(),
-            text: 'revisar Neuromodulação',
-            isCompleted: false
-        },
-        {
-            id: uuidv4(),
-            text: 'flashcard sobre Biofeedback e Eletromiografia',
-            isCompleted: false
-        },
-        {
-            id: uuidv4(),
-            text: 'flashcard sobre Fibromialgia',
-            isCompleted: false
-        }
-    ])
+    // const todosArray =  [{
+    //     id: uuidv4(),
+    //     text: 'Estudar Dor Pélvica Crônica',
+    //     isCompleted: false
+    // },
+    // {
+    //     id: uuidv4(),
+    //     text: 'Revisar Incontinência Urinária e Disfunções Pélvicas',
+    //     isCompleted: false
+    // },
+    // {
+    //     id: uuidv4(),
+    //     text: 'Assistir 2 aulas do curso Gestão da Dor no Câncer Ginecológico',
+    //     isCompleted: false
+    // },
+    // {
+    //     id: uuidv4(),
+    //     text: 'Revisar Neuromodulação',
+    //     isCompleted: false
+    // },
+    // {
+    //     id: uuidv4(),
+    //     text: 'Flashcard sobre Biofeedback e Eletromiografia',
+    //     isCompleted: false
+    // },
+    // {
+    //     id: uuidv4(),
+    //     text: 'Flashcard sobre Fibromialgia',
+    //     isCompleted: false
+    // }]
+    
+    let listaTodos = JSON.parse(localStorage.getItem('todos'))
+    
+    const [todos, setTodos] = useState(listaTodos
+       
+    )
 
+    localStorage.setItem('todos', JSON.stringify(todos))
     const [filter, setFilter] = useState("All")
 
     let countTodo = 0;
@@ -73,8 +79,6 @@ const TodoForm = () => {
     }
 
     function handleDragEnd(event) {
-        console.log("Drag and drop funfando")
-        console.log(event)
         if (event.delta.x === 0) {
             if (event.active.id === event.over.id) {
                 if (event.activatorEvent.target.className === 'complete' || event.activatorEvent.target.nodeName === 'P') {
@@ -104,7 +108,9 @@ const TodoForm = () => {
             <div className='todo-list'>
                 <DndContext
                     collisionDetection={closestCenter}
+
                     onDragEnd={handleDragEnd}
+                // sensors={sensors}
                 >
                     <SortableContext
                         items={todos}
